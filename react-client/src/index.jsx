91ea4a498fb1f0affe68d2fce738783c5d4b686c
Aric Alves <aric.alves@yahoo.com>
2018-01-26 15:16:21 -0800

    (Init)

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,34 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import $ from 'jquery';
-import List from './components/List.jsx';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { 
-      items: []
-    }
-  }
-
-  componentDidMount() {
-    $.ajax({
-      url: '/items', 
-      success: (data) => {
-        this.setState({
-          items: data
-        })
-      },
-      error: (err) => {
-        console.log('err', err);
-      }
-    });
-  }
 
   render () {
     return (<div>
-      <h1>Item List</h1>
-      <List items={this.state.items}/>
+      <h1>Hello</h1>
     </div>)
   }
 }
