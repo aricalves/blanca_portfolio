3b9e5bff7d360c9f28593416c0d1a9ceaa6dd302
Aric Alves <aric.alves@yahoo.com>
2018-01-26 17:28:38 -0800

    Add components dir and NameCard

---
+++ react-client/src/components/NameCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import styles from '../styles';
+
+const NameCard = () => (
+  <h1>Blanca Ponce</h1>
+);
+
+export default NameCard;
